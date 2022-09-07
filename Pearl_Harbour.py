d0fe7cfc67dd69c32dfbedaa38cbc8d27f06cb93
al lisyatnikov <a.lisyatnikov@gmail.com>
2022-09-07 20:33:37 +0300

    making idea of the enemy

--- Pearl_Harbour.py
+++ Pearl_Harbour.py
@@ -20,6 +20,31 @@ class Shell:
 
 
 class Enemy:
+    def __init__(self):
+        pass
+
+    def move(self):
+        pass
+
+    def release_bomb(self):
+        pass
+
+    def open_gunfire(self):
+        pass
+
+    def release_missile(self):
+        pass
+
+
+class Enemy_Bomb():
+    pass
+
+
+class Enemy_Shell():
+    pass
+
+
+class Enemy_Missile():
     pass
 
 
