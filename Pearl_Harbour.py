a5301315342b68616ec8d7cd33e60fee35dd63ca
al lisyatnikov <a.lisyatnikov@gmail.com>
2022-09-07 20:17:05 +0300

    making idea

--- Pearl_Harbour.py
+++ Pearl_Harbour.py
@@ -1,8 +1,23 @@
 class Anti_Aircraft_Gun:
-    pass
+    def __init__(self):
+        pass
+
+    def move_platform(self):
+        pass
+
+    def change_fire_direction(self):
+        pass
+
+    def open_fire(self):
+        pass
 
 class Shell:
-    pass
+    def __init__(self):
+        pass
+
+    def detect_impact(self):
+        pass
+
 
 class Enemy:
     pass
